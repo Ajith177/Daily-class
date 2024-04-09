e5ffad04b500b486d927fd4987302c038619bd4f
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2024-04-09 18:42:43 +0530

    Add files via upload

---
+++ List-Methods.py
@@ -0,0 +1,58 @@
+#Sort  Method 
+
+# List Methods....
+
+a=["bala","king","ajith","saran"]
+a.sort()
+print(a)
+
+b=[4,7,2,67,12,4,9,4]
+b.sort()
+print(b)
+
+
+b.remove(4)  #Remove the First Occurence
+print(b)
+
+c=[78,456,8674,4353]
+b.extend(c) #add Multiple Items in a List.
+print(b)
+
+
+print(c,"c")
+c.clear() # Clear all the elements in the List.
+print(c)
+
+s="ajith"
+d="ganapathy"
+print(s+d) # Concatenation we can add both the Values.
+
+q=4 
+w=3 
+print(q+w)
+
+e=[3,5,6,1,0]
+e.reverse()
+print(e) # reverse the List.
+
+
+#Append the element in the List.
+e.append("ajith")
+print(e)
+
+
+
+t=10,20,30
+k=t
+print(t)
+print(type(t))
+
+
+print(id(t))
+print(id(k))
+
+l=90,89 
+print(id(l))
+
+
+
