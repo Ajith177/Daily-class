f73abe842e317bbbff3de2a4627b0e56570b43f5
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2024-04-15 19:45:30 +0530

    Add files via upload

---
+++ 15-4-24 Python Operators.py
@@ -0,0 +1,22 @@
+# Operators In Python.
+
+# Arithmetic, Logical & Relational Operators are Important
+
+# Arithmetic includes = +,-,*,Division
+
+# Relational => >,<.>=,<= 
+
+# Logical => And,or, not
+
+# a=int(input("enter the Number for a:"))
+# b=int(input("enter the Number for b:"))
+# c=int(input("enter the Number for c:"))
+# min=a if  a<b and a<c else b if b<c else c
+# print("minimum value",min)
+
+
+k=19
+l=19
+print(k is l)
+
+
