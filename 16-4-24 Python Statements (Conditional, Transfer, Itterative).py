89bd8b27c5d4c5862d67c18d96b67507ac068f9c
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2024-04-16 13:14:32 +0530

    Add files via upload

---
+++ 16-4-24 Python Statements (Conditional, Transfer, Itterative).py
@@ -0,0 +1,50 @@
+# Conditional Statements
+# if, elif, else
+
+# n=input("enter a name:")
+# if(n=="ajith"):
+#     print("matched")
+# elif(n=="saran"):
+#     print("it is saran")
+# else:
+#     print("it is susmitha")
+    
+    
+# Giving the Direct Values..
+
+# a=4
+# b=10
+# if(a>b):
+#     print("a is Greater")
+# else:
+#     print("B is greater")
+
+
+
+# For Loop  => It iterated the Sequence of Character => It will work in Starings, Tuples, Lists, & Dictionary..
+
+# k="ajith ganapathy"
+# for i in k:
+#     print(i)
+
+
+
+# Using While Loop.
+
+a=1
+while(a<=10):
+    print("the numbers are:",a)
+    a=a+1
+    
+# While loop will run until the Given Condition is Met.
+
+
+
+# Transfer Statements => Break, Continue, & Pass.
+# Break Statements
+
+for i in range(10):
+    if i==4:
+        break
+    print(i)
+
