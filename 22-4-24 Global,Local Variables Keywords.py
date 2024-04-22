a5ef86438c815f57cf694f525fa14ecbcee5ac50
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2024-04-22 12:41:36 +0530

    Add files via upload

---
+++ 22-4-24 Global,Local Variables Keywords.py
@@ -0,0 +1,65 @@
+# Variables => They were used to Store the Data..
+
+# There are of two types of Variables..
+#  -> Global Variables
+# -> Local Variables
+
+# Example of Local Variables........
+
+a="ajithganapaty"
+def ajith(e):
+    print(e,"is a good Boy")
+    print(a)
+e="ajith"
+ajith(e)
+
+# Example of the Local Variables.....
+
+def zybisys(k):
+    l="saranraj"
+    print(k,"is working on Zybisys as a Tester")
+k="ajith"
+zybisys(k)
+# print(l)
+
+
+# Global Keyword..
+
+# a=98
+# def s():
+#     global a 
+#     a=56
+#     print(a)
+# def f3():
+#     print(a)
+# s()
+# f3()
+
+
+
+# Lambda Function
+
+# q=lambda n:n**n 
+# print(q(5))
+
+# Lambda Function is a Single Line Statement of getting the output..
+
+# Recursive Function.......
+# result = 1 
+
+# def factorial(n):
+#     if(n==0):
+#         result = 1
+#     else:
+#         result=n*factorial(n-1)
+#     return result
+# print(result)
+# # n=4
+# factorial(4)
+
+# f=7
+# def s():
+#     print(f)
+# s()
+
+
