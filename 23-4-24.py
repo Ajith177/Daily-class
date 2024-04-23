6e2e9bf9e9414df5cdfa2046bdd15f94a6734b36
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2024-04-23 19:45:33 +0530

    Add files via upload

---
+++ 23-4-24.py
@@ -0,0 +1,36 @@
+# Map () Function
+
+l=[1,2,3,4]
+k=tuple(map(lambda s:2*s,l))
+print(k)
+
+
+#Function Aliasing..
+
+# def ajith(n):
+#     print(n,"is a Good Boy")
+# n="ajithganapathy"
+# saran=ajith 
+# saran(n)
+
+
+# def ajith(c):
+#     print(c)
+# c="varata"
+# ajith(c)
+
+
+# Concepts Learned Today..
+
+# 1)Map() Function
+# 2)Nested Function
+# 3)Conditional Statements
+# 4)Function Aliasing
+# 5)Reduce () Function
+
+
+
+
+
+
+
