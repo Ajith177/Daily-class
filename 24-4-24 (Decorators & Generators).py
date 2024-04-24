f2aaaaecdfef382bb71756b71b7e8b2b5735955d
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2024-04-24 11:13:28 +0530

    Add files via upload

---
+++ 24-4-24 (Decorators & Generators).py
@@ -0,0 +1,34 @@
+# Function Decorators:
+
+
+# def f1(func):
+#     def wrappper():
+#         print("Started")
+#         func()
+#         print("Ended")
+#     return wrappper 
+    
+# def f():
+#     print("Hello")
+
+# f1(f)()
+
+# Generator 
+# Responsible of  Generating  the Sequence of Values.  with the Keyword => "Yield"
+
+
+# Yield Acting Like a Return Statement
+
+# Next Keyword is used To Print the Values here.
+
+def ajith():
+    yield "ajith"
+    yield "Saran"
+
+k=ajith()
+print(type(k))
+
+
+
+
+
