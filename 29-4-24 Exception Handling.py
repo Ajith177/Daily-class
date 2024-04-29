8c08086f8d3e6323f492ad26b2a671db55de544f
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2024-04-29 14:37:12 +0530

    Add files via upload

---
+++ 29-4-24 Exception Handling.py
@@ -0,0 +1,47 @@
+# Exception Handling
+
+a=10
+if a==10
+    print("ajith")
+
+It is of two types they were:
+Syntax error
+Runtime error
+
+
+# With Try-Except
+
+print("ajith")
+try:
+    print(10/0)
+except ZeroDivisionError:
+    print("saran")
+print("susmitha")
+
+
+print("Executing the Type-error")
+print("Zybisys")
+try:
+    print(1+"2")
+except  TypeError:
+    print("Successfully Executed the Type-error")
+print("done")
+
+
+print("Executing the Control-flow in Try")
+try:
+    print(10/0)
+    print("ajith")
+    print(7/0)
+except ZeroDivisionError:
+    print("welcome")
+print("bye")
+
+
+# print(int("10"))
+# print(int("ajith"))
+
+# Value-error
+print("executing the Value-error")
+
+
