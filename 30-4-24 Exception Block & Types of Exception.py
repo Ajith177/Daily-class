f8ea1e3dd83613772cb335d3bbd4103df831af0d
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2024-04-30 11:30:00 +0530

    Add files via upload

---
+++ 30-4-24 Exception Block & Types of Exception.py
@@ -0,0 +1,20 @@
+# Exception Blocks
+
+# They were three Blocks in the Exception they were:
+#     try block,
+#     Except block
+#     Final block
+    
+    
+try:
+    print("ajith")
+    print(10/0)
+    print("saran")
+except ZeroDivisionError:
+    print("susmitha")
+print("Thankyou")
+
+# Types of Exception:
+# 1)Predefined Exception
+# 2)User-defined Exception
+
